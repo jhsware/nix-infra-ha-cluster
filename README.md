89f306f563bd618510f50aff41f8c44a65b6b00e
Sebastian Ware <sebastian@urbantalk.se>
2024-10-27 14:00:44 +0100

    Fix

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
-# nix-infra-ha-cluster
+# Cluster Template
+
+This is a high availability cluster configuration.
+
+1. Configure the OpenSSL-templates:
+
+[] openssl/opensslCnfCaOrig.cnf
+[] openssl/openSslCnfInterOrig.cnf
+
+2. Configure the cluster node configurations (add more if needed):
+
+[] nodes/*.nix
+
+Then you can init your cluster, provision cloud nodes and deploy your cluster. When the cluster is deployed you need to configure your applications.
+
+3. Add your application modules
+
+[] app_modules/*
+[] update app_modules/default.nix
+
+Eventually you will want to direct your domains to the ingress node.
+
+4. Configure ingress
+
+[] domain CNAME to ingress node
+[] nodes/ingress001.nix
--- nodes/worker001.nix
+++ nodes/worker001.nix
@@ -11,17 +11,17 @@
 
   config.infrastructure.podman.dockerRegistryHostPort = "[%%registry001.overlayIp%%]:5000";
 
-  config.infrastructure.app-pod = {
-    enable = true;
-    bindToIp = "[%%localhost.overlayIp%%]";
-    secretName = "[%%secrets/my.test%%]";
-  };
+  # config.infrastructure.app-pod = {
+  #   enable = true;
+  #   bindToIp = "[%%localhost.overlayIp%%]";
+  #   secretName = "[%%secrets/my.test%%]";
+  # };
 
-  config.infrastructure.app-mongodb-pod = {
-    enable = true;
-    bindToIp = "[%%localhost.overlayIp%%]";
-    mongodbConnectionString = "mongodb://[%%service001.overlayIp%%]:27017,[%%service002.overlayIp%%]:27017,[%%service003.overlayIp%%]:27017/test?replicaSet=rs0&connectTimeoutMS=1000";
-  };
+  # config.infrastructure.app-mongodb-pod = {
+  #   enable = true;
+  #   bindToIp = "[%%localhost.overlayIp%%]";
+  #   mongodbConnectionString = "mongodb://[%%service001.overlayIp%%]:27017,[%%service002.overlayIp%%]:27017,[%%service003.overlayIp%%]:27017/test?replicaSet=rs0&connectTimeoutMS=1000";
+  # };
 
   config.networking.firewall.interfaces."flannel-wg".allowedTCPPorts = [ 11211 11311 ];
 }
