d2cdcf5c4030a8f4aaf02abb33370113074f38b5
Sebastian Ware <sebastian@urbantalk.se>
2025-02-15 13:50:07 +0100

    disable harmonia

--- nodes/registry001.nix
+++ nodes/registry001.nix
@@ -1,37 +1,37 @@
 { config, pkgs, lib, ... }: {
   config = {
-    services.harmonia = {
-      # On first install you need generate a key pair
-      # 
-      #   $ nix-store --generate-binary-cache-key cache.flstr.cloud certs/cache.secret.pem certs/cache.pub.pem
-      # 
-      # Then copy the public key to the client node configuration
-      #
-      #   config.nix = {
-      #     settings = {
-      #       substituters = [
-      #         "http://[%%registry001.overlayIp%%]:1099"
-      #         "https://cache.nixos.org/"
-      #       ];
-      #       trusted-public-keys = [%%nix-store.trusted-public-keys%%];
-      #     };
-      #   };
-      #
-      enable = true;
-      signKeyPaths = [
-        "/root/certs/cache.secret.pem"
-      ];
-      settings = {
-        # default ip:hostname to bind to
-        bind = "[%%localhost.overlayIp%%]:1099";
-        # Sets number of workers to start in the webserver
-        workers = 4;
-        # Sets the per-worker maximum number of concurrent connections.
-        max_connection_rate = 256;
-        # binary cache priority that is advertised in /nix-cache-info
-        priority = 30;
-      };
-    };
+    # services.harmonia = {
+    #   # On first install you need generate a key pair
+    #   # 
+    #   #   $ nix-store --generate-binary-cache-key cache.flstr.cloud certs/cache.secret.pem certs/cache.pub.pem
+    #   # 
+    #   # Then copy the public key to the client node configuration
+    #   #
+    #   #   config.nix = {
+    #   #     settings = {
+    #   #       substituters = [
+    #   #         "http://[%%registry001.overlayIp%%]:1099"
+    #   #         "https://cache.nixos.org/"
+    #   #       ];
+    #   #       trusted-public-keys = [%%nix-store.trusted-public-keys%%];
+    #   #     };
+    #   #   };
+    #   #
+    #   enable = true;
+    #   signKeyPaths = [
+    #     "/root/certs/cache.secret.pem"
+    #   ];
+    #   settings = {
+    #     # default ip:hostname to bind to
+    #     bind = "[%%localhost.overlayIp%%]:1099";
+    #     # Sets number of workers to start in the webserver
+    #     workers = 4;
+    #     # Sets the per-worker maximum number of concurrent connections.
+    #     max_connection_rate = 256;
+    #     # binary cache priority that is advertised in /nix-cache-info
+    #     priority = 30;
+    #   };
+    # };
     services.dockerRegistry = {
       # https://distribution.github.io/distribution/
       # curl -I -k -s http://10.10.93.0:5000/ | head -n 1 | cut -d ' ' -f 2
